e0521ad240fd39e1d923fbfe52adc5cc570f730a
hutao0723 <1476485904@qq.com>
2019-08-09 17:46:55 +0800

    添加icon组件

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
+import Icon from "./components/icon.vue";
 
 new Vue({
     el:"#app",
+    components:{
+        "s-icon":Icon,
+    }
 })
---
+++ src/components/icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <i :class="['iconfont',`icon-${name}`]"></i>
+</template>
+
+<script>
+export default {
+    name:'icon' ,
+    props:["name"]
+}
+</script>
+
+<style lang="less" scoped>
+@import url('//at.alicdn.com/t/font_864940_k4v9os5xc5.css');
+
+</style>
